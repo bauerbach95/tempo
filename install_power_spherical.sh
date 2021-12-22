1269e47c80377edb53434186a0fc3acb0b661311
bauerbach95 <bauerbach95@gmail.com>
2021-12-21 20:20:15 -0500

    update install_power_spherical.sh

--- install_power_spherical.sh
+++ install_power_spherical.sh
@@ -1,8 +1,8 @@
-git clone https://github.com/nicola-decao/power_spherical
+git clone https://github.com/nicola-decao/power_spherical.git
 cd power_spherical
 python setup.py install
 cd ..
-git clone https://github.com/nicola-decao/s-vae-pytorch
+git clone https://github.com/nicola-decao/s-vae-pytorch.git
 cd s-vae-pytorch
 python setup.py install
 cd ..
